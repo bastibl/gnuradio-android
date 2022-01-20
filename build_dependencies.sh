7e7d5da56c00f2ac27b567586d1e03fc5385255e
Adrian Suciu <adrian.suciu@analog.com>
2022-01-20 22:40:11 +0200

    build_dependencies: do not build thrift and libzmq
    Signed-off-by: Adrian Suciu <adrian.suciu@analog.com>

--- build_dependencies.sh
+++ build_dependencies.sh
@@ -9,10 +9,10 @@ build_libiconv # HANDLE CIRCULAR DEP
 build_libxml2
 build_boost
 move_boost_libs
-build_libzmq
+#build_libzmq
 build_fftw
 build_openssl
-build_thrift
+#build_thrift
 build_libgmp
 build_libusb
 build_libiio
