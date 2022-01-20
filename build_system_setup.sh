ff92dfc72d1ca46790c2d0188131db60b0a2fb1a
Adrian Suciu <adrian.suciu@analog.com>
2022-01-20 22:41:23 +0200

    Update build_system_setup.sh

--- build_system_setup.sh
+++ build_system_setup.sh
@@ -4,6 +4,7 @@
 export USER_DIR=$1
 export ANDROID_SDK_ROOT=$HOME/Android/Sdk
 export CMAKE=$HOME/Qt/Tools/CMake/bin/cmake
-export QT_INSTALL_PREFIX=$HOME/Qt/5.15.2/android
-export JDK=$HOME/jdk-16.0.2
-export BUILD_ROOT=/home/adi/src/scopy-android-deps/gnuradio-android
+export SCRIPT_HOME_DIR=$HOME/src/scopy-android-deps
+export DEPS_SRC_PATH=$SCRIPT_HOME_DIR/downloads
+export BUILD_ROOT=$SCRIPT_HOME_DIR/gnuradio-android
+export BUILD_TYPE=Debug
